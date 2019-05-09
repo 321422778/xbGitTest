b250acc20f0d7b22b7294dc18f3db732826df6b3
VOKDA <321422778@qq.com>
2019-05-09 21:24:06 +0800

    添加version方法，还有一段注释

--- src/main/java/iamxb.java
+++ src/main/java/iamxb.java
@@ -1,5 +1,9 @@
 public class iamxb {
     public static void main(String[] args) {
-        System.out.println("我是学霸君");
+        System.out.println("我是学霸君，测试一下");
+    }
+    public void version()
+    {
+        //这只是一个注释而已
     }
 }
